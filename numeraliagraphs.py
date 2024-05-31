636ac17d2c907d3743d3ae948c6a2f1a4dff72ae
Israel Monjaraz <111941844+13260618@users.noreply.github.com>
2024-05-31 13:51:02 -0600

    Update numeraliagraphs.py

--- numeraliagraphs.py
+++ numeraliagraphs.py
@@ -1,15 +1,15 @@
 import plotly.graph_objects as go
 
-# Datos de ejemplo para el mapa de México
+
 mexico_states = ['Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua',
                  'Coahuila', 'Colima', 'Durango', 'Guanajuato', 'Guerrero', 'Hidalgo', 'Jalisco', 'México', 'Michoacán',
                  'Morelos', 'Nayarit', 'Nuevo León', 'Oaxaca', 'Puebla', 'Querétaro', 'Quintana Roo', 'San Luis Potosí',
                  'Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatán', 'Zacatecas']
 
-# Valores de ejemplo para cada estado (puedes reemplazarlos con tus propios datos)
+
 values = [10, 20, 15, 30, 25, 35, 40, 20, 10, 5, 15, 25, 30, 20, 10, 25, 20, 35, 30, 25, 20, 15, 10, 25, 30, 20, 15, 10, 5, 20, 15]
 
-# Crear el mapa de México
+
 fig = go.Figure(data=go.Choropleth(
     locations=mexico_states,  # Nombre de los estados
     z=values,  # Valor para cada estado
